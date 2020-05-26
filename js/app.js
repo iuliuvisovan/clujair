3df5f40004755022d92564e0e13a95f434db0369
Iuliu Visovan <iuliuvisovan@usource.ro>
2020-05-27 00:24:19 +0300

    im happy so far

--- js/app.js
+++ js/app.js
@@ -1,25 +1,26 @@
+let allClujDevices = [];
+
 async function getAndDisplayData() {
-  neighbourhoods
-    .filter((x) => x.shouldShow)
-    .forEach(({ id, name }) => {
-      const neighbourhoodNameElement = document.querySelector(`#${id} .name`);
-
-      neighbourhoodNameElement.innerText = name;
-    });
-
+  drawNeighborhoods();
   await getAndPopulateClujDevices();
+  drawNeighborhoods();
+}
+
+function drawNeighborhoods() {
+  const pageRoot = document.getElementById('root');
+
+  pageRoot.innerHTML = '';
 
   neighbourhoods
     .filter((x) => x.shouldShow)
-    .forEach(({ id, name }) => {
-      const neighbourhoodBodyElement = document.querySelector(`#${id}`);
-      const neighbourhoodValueElement = document.querySelector(`#${id} .current-value`);
+    .forEach(({ id, name, imageUrl }) => {
+      const overallGrade = getAirQualityForNeighbourhood(id);
 
-      const grade = getAirQualityForNeighbourhood(id);
+      const { pm1, pm25, pm10 } = getPmAveragesForNeighborhood(id);
 
-      const neighbourhoodAirQuality = grade;
-      neighbourhoodValueElement.innerText = neighbourhoodAirQuality;
-      neighbourhoodBodyElement.style.backgroundColor = getColor(grade);
+      const neighbourhoodHtml = getHtmlForNeighbourhood({ id, name, value: overallGrade, imageUrl, pm1, pm25, pm10 });
+
+      pageRoot.insertAdjacentHTML('beforeend', neighbourhoodHtml);
     });
 }
 
@@ -45,6 +46,10 @@ function getAirQualityForNeighbourhood(neighbourhoodId) {
   const deviceIdsInNeighbourhood = neighbourhoods.find((x) => x.id == neighbourhoodId).deviceIds;
   const devicesInNeighbourhood = allClujDevices.filter((x) => deviceIdsInNeighbourhood.includes(x.id));
 
+  if (!devicesInNeighbourhood.length) {
+    return '';
+  }
+
   let sumForNeighbourhood = 0;
 
   devicesInNeighbourhood.forEach(({ avg_pm1, avg_pm25, avg_pm10 }) => {
@@ -62,16 +67,41 @@ function getAirQualityForNeighbourhood(neighbourhoodId) {
   return grade.toFixed(1);
 }
 
+function getPmAveragesForNeighborhood(neighbourhoodId) {
+  const deviceIdsInNeighbourhood = neighbourhoods.find((x) => x.id == neighbourhoodId).deviceIds;
+  const devicesInNeighbourhood = allClujDevices.filter((x) => deviceIdsInNeighbourhood.includes(x.id));
+
+  let averages = {
+    pm1: 0,
+    pm25: 0,
+    pm10: 0,
+  };
+
+  devicesInNeighbourhood.forEach(({ avg_pm1, avg_pm25, avg_pm10 }) => {
+    averages.pm1 += +avg_pm1;
+    averages.pm25 += +avg_pm25;
+    averages.pm10 += +avg_pm10;
+  });
+
+  return {
+    pm1: averages.pm1 / devicesInNeighbourhood.length,
+    pm25: averages.pm25 / devicesInNeighbourhood.length,
+    pm10: averages.pm10 / devicesInNeighbourhood.length,
+  };
+}
+
 const neighbourhoods = [
   {
     id: 'grigorescu',
     name: 'Grigorescu',
+    imageUrl: 'https://www.clujlife.com/wp-content/uploads/2018/08/cartier-grigorescu1-1.jpg',
     deviceIds: ['160000C7'],
     shouldShow: true,
   },
   {
     id: 'plopilor',
-    name: 'Mănăștur',
+    name: 'Plopilor',
+    imageUrl: 'https://i0.wp.com/cluju.ro/wp-content/uploads/2018/01/13475165_1800244116865752_2793115031474893207_o.jpg?fit=940%2C528&ssl=1',
     deviceIds: ['160000CB'],
     shouldShow: true,
   },
@@ -79,16 +109,20 @@ const neighbourhoods = [
     id: 'manastur',
     name: 'Mănăștur',
     deviceIds: ['160000CA'],
+    imageUrl: 'https://i0.wp.com/cluju.ro/wp-content/uploads/2018/01/13475165_1800244116865752_2793115031474893207_o.jpg?fit=940%2C528&ssl=1',
   },
   {
     id: 'europa',
     name: 'Europa',
     deviceIds: ['160000FA', '160000C6', '820001CF'],
+    imageUrl: 'https://i.imgur.com/K1YKwqc.png',
+    shouldShow: true,
   },
   {
     id: 'bunaziua',
     name: 'Bună Ziua',
     deviceIds: ['160000D3', '160000A2', '160000A5'],
+    imageUrl: 'https://ochiulclujean.ro/wp-content/uploads/2020/05/97993445_2628164784063791_8408341605198594048_n-960x640.jpg',
     shouldShow: true,
   },
   {
@@ -100,9 +134,59 @@ const neighbourhoods = [
 
 getAndDisplayData();
 
-let allClujDevices = undefined;
+function getHtmlForNeighbourhood({ id, name, value, imageUrl, pm1, pm25, pm10 }) {
+  return `
+    <div class="neighborhood" id="${id}" style="background-color: ${getColor(value)}">
+    <div class="header">
+      Cartierul&nbsp;
+      <span class="name">${name}</span>
+    </div>
+    <div class="body">
+      <span class="current-value">${value}</span>
+      <div class="info">
+        ${getHtmlForProgressBar({ name: 'PM1.0', value: pm1, legalValue: 20 })}
+        ${getHtmlForProgressBar({ name: 'PM2.5', value: pm25, legalValue: 25 })}
+        ${getHtmlForProgressBar({ name: 'PM10', value: pm10, legalValue: 40 })}
+      </div>
+      <div class="gradient"></div>
+      <img
+        src="${imageUrl}"
+        style="height: 280px;"
+        alt="Cartierul ${name}"
+      />
+    </div>
+  </div>
+    `;
+}
 
-const getColor = (value) => {
+function getHtmlForProgressBar({ name, value, legalValue }) {
+  let percentage = (value / legalValue) * 100;
+  if (percentage < 15) {
+    percentage = 15;
+  }
+
+  return `
+    <div class="progress-bar-wrapper">
+      <div class="progress-bar">
+        <div class="progress" style="background-color: ${getColor(13 - (value / legalValue) * 10)}; width: ${percentage}%">
+          <div class="progress-value">
+            ${(value || 0).toFixed(1)}
+          </div>
+        </div>
+      </div>
+      <div class="progress-bar-name">
+        ${name}
+      </div>
+      <div class="progress-bar-max-value">/ <b>${legalValue}</b> µg/m³</div>
+    </div>
+    `;
+}
+
+function getColor(value) {
+  if (!value) {
+    return '';
+  }
+
   if (value >= 9.5) {
     return '#4ac355';
   }
@@ -153,4 +237,4 @@ const getColor = (value) => {
   }
 
   return '#000';
-};
+}
